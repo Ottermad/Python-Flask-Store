336b965051e109b0b90d626b08b0880bcfaf641c
Ottermad <ClaireThomas@ATLTTSC855.AttwoodThomas.local>
2014-12-30 15:04:52 +0000

    commit

--- app.py
+++ app.py
@@ -4,5 +4,143 @@ import requests
 
 app = Flask(__name__)
 
+def get_list_view_html(product_id, product):
+    
+    output = ""
+
+    output = output + "<li>";
+    output = output + '<a href="shirt.php?id=' + $product_id + '">';
+    output = output + '<img src="' . $product["img"] . '" alt="' . $product["name"] . '">';
+    output = output + "<p>View Details</p>";
+    output = output + "</a>";
+    output = output + "</li>";
+
+    return $output;
+}
+
+$products = array();
+$products[101] = array(
+	"name" => "Logo Shirt, Red",
+	"img" => "img/shirts/shirt-101.jpg",
+	"price" => 18,
+	"paypal" => "9P7DLECFD4LKE",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[102] = array(
+	"name" => "Mike the Frog Shirt, Black",
+    "img" => "img/shirts/shirt-102.jpg",
+    "price" => 20,
+    "paypal" => "SXKPTHN2EES3J",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[103] = array(
+    "name" => "Mike the Frog Shirt, Blue",
+    "img" => "img/shirts/shirt-103.jpg",    
+    "price" => 20,
+    "paypal" => "7T8LK5WXT5Q9J",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[104] = array(
+    "name" => "Logo Shirt, Green",
+    "img" => "img/shirts/shirt-104.jpg",    
+    "price" => 18,
+    "paypal" => "YKVL5F87E8PCS",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[105] = array(
+    "name" => "Mike the Frog Shirt, Yellow",
+    "img" => "img/shirts/shirt-105.jpg",    
+    "price" => 25,
+    "paypal" => "4CLP2SCVYM288",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[106] = array(
+    "name" => "Logo Shirt, Gray",
+    "img" => "img/shirts/shirt-106.jpg",    
+    "price" => 20,
+    "paypal" => "TNAZ2RGYYJ396",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[107] = array(
+    "name" => "Logo Shirt, Teal",
+    "img" => "img/shirts/shirt-107.jpg",    
+    "price" => 20,
+    "paypal" => "S5FMPJN6Y2C32",
+    "sizes" => array("Small","Medium","Large","X-Large")
+);
+$products[108] = array(
+    "name" => "Mike the Frog Shirt, Orange",
+    "img" => "img/shirts/shirt-108.jpg",    
+    "price" => 25,
+    "paypal" => "JMFK7P7VEHS44",
+    "sizes" => array("Large","X-Large")
+);
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/")
+def index():
+	pass
+
+@app.route("/shirts")
+def shirts():
+	pass
+
+@app.route("/shirt")
+def shirt():
+	pass
+
+@app.route("/receipt")
+def receipt():
+	pass
+
+@app.route("/contact")
+def contact():
+	pass
+
+@app.route("/products")
+def products():
+	pass
+
 if __name__ == "__main__":
 	app.run(debug=True)
